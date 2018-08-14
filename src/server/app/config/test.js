b80eedc34d97343efe1d2788b7ed9c6abbfa62cf
xrobert <robert-x@mgdis.fr>
2018-08-14 15:08:19 +0200

    feat(config): update test configuration

--- src/server/app/config/test.js
+++ src/server/app/config/test.js
@@ -1,5 +1,5 @@
 const config = {
-  MONGO_URL: process.env.MONGO_URL || '192.168.198.130',
+  MONGO_URL: process.env.MONGO_URL || 'localhost',
   MONGO_PORT : process.env.MONGO_PORT || 27017,
   LOG_LEVEL: process.env.LOG_LEVEL || "info",
   SWAGGER_ACTIVATED: process.env.SWAGGER_ACTIVATED || false,
